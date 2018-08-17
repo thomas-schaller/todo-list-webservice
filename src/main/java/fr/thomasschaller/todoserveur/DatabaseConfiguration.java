90f47828b50360a22566a35306113686b632a2d1
Thomas Schaller <41308408+thomas-schaller@users.noreply.github.com>
2018-08-17 11:35:40 +0800

    Mise en place d'une base de données Postgres. L'url jdbc est défini par une variable d'environnement. Cela fonctionne sur un serveur heroku.

---
+++ src/main/java/fr/thomasschaller/todoserveur/DatabaseConfiguration.java
@@ -0,0 +1,22 @@
+package fr.thomasschaller.todoserveur;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
+
